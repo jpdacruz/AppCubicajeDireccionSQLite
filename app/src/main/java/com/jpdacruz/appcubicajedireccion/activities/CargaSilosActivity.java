ad4f565ff8d7a194b05ad39b459da7cbd42cc073
jpdacruz <jpdacruz@hotmail.com>
2020-01-21 11:44:17 -0300

    ahora voy a tratar de hacer una interface general

--- app/src/main/java/com/jpdacruz/appcubicajedireccion/activities/CargaSilosActivity.java
+++ app/src/main/java/com/jpdacruz/appcubicajedireccion/activities/CargaSilosActivity.java
@@ -190,6 +190,44 @@ public class CargaSilosActivity extends AppCompatActivity implements
                 cubicajeSiloString = "";
             }
         });
+
+        mCono.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                mToneladasSilo.setText("");
+                cubicajeSiloString = "";
+            }
+        });
+
+        mCopete.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                mToneladasSilo.setText("");
+                cubicajeSiloString = "";
+            }
+        });
     }
     private void iniciarBotonesListener() {
 
