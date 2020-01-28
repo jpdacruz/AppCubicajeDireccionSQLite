496e510c630f2bbdd0e298df60fcb824b9300bc3
jpdacruz <jpdacruz@hotmail.com>
2020-01-28 20:28:06 -0300

    corregido mostrar suma granos

---
+++ app/src/main/java/com/jpdacruz/appcubicajedireccion/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.jpdacruz.appcubicajedireccion.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jpdacruz.appcubicajedireccion.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
