47042a8469689633802c78ac0162d9a4a94f25dd
jpdacruz <jpdacruz@hotmail.com>
2020-01-30 12:12:23 -0300

    concha de tu madre merge

--- app/src/main/java/com/jpdacruz/appcubicajedireccion/activities/SplashActivity.java
+++ app/src/main/java/com/jpdacruz/appcubicajedireccion/activities/SplashActivity.java
@@ -1,38 +1,21 @@
 package com.jpdacruz.appcubicajedireccion.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-<<<<<<< Updated upstream
 import android.os.Bundle;
-
-=======
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Toast;
-
-import com.jpdacruz.appcubicajedireccion.MainActivity;
->>>>>>> Stashed changes
 import com.jpdacruz.appcubicajedireccion.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-<<<<<<< Updated upstream
-=======
     ProgressBar progressBar;
 
 
->>>>>>> Stashed changes
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-<<<<<<< Updated upstream
-=======
 
         progressBar = findViewById(R.id.progressBar);
 
@@ -46,6 +29,5 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         },5000);
->>>>>>> Stashed changes
     }
 }
---
+++ app/src/main/java/com/jpdacruz/appcubicajedireccion/clases/InterfaceGeneral.java
@@ -0,0 +1,6 @@
+package com.jpdacruz.appcubicajedireccion.clases;
+
+public interface InterfaceGeneral {
+
+    Double formatearDecimales(Double numero, Integer numeroDecimales);
+}
